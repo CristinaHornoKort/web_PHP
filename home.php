1fa101275838662fd0ff4037456837cd513cc759
Cristina Horno <cristina.horno@gmail.com>
2017-06-26 18:40:51 +0200

    commit home.php

---
+++ home.php
@@ -0,0 +1,15 @@
+<div id="content">
+
+	<div id="left-col">
+		<h2>Geology Map</h2> 
+		<img src="images/geo_map_1.jpg" alt="geo_map_1" />
+	</div>
+	
+	<div id="right-col">
+		<h2>Geology</h2>
+		
+		<p>The geology of the Maltese Islands is relatively young when considered within a geological time frame, with the oldest rock dating back only to the Tertiary period. The Islands are for the most part composed of marine sedimentary rocks. Although the sedimentary platform on which the Maltese Islands are situated was formed during the Triassic, there are no surface outcrops of this age. All exposed rocks were deposited during the Oligocene and Miocene periods of geological time dating back to some 30 to 35 million years ago. The most recent deposits are the quaternary deposits which are found in minor quantities and are of terrestrial origin. The resultant rock formations are consisting of five basic layers laid on top of the other in a layer-cake sequence.</p>
+		
+	</div>
+</div>
+<div class="clear"></div>

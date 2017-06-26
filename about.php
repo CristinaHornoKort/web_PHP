03dc01afe3c51977ec45689b51a5c33a876c20ce
Cristina Horno <cristina.horno@gmail.com>
2017-06-26 18:42:39 +0200

    commit about.php

---
+++ about.php
@@ -0,0 +1,10 @@
+<div id="content">
+	<?php
+		//array
+		$maps = array ("Base map", "Satellite map", "Topographic map", "Physical map", "Geological map", "Hydrological map");
+		//foreach loop
+		foreach ($maps as $x){
+			echo $x."</br>";
+		}
+	?>
+<div class="clear"></div>
